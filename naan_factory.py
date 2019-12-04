5bd7c6c3256905af6a3d30d3cd3c8fcd7e0b3e95
Thomas Nguyen <TNguyen@spartaglobal.com>
2019-12-04 15:08:36 +0000

    All tests passing

--- naan_factory.py
+++ naan_factory.py
@@ -17,7 +17,10 @@ def make_dough(arg1, arg2):
         return 'dough'
 
 def bake_dough(arg1):
-    return 'Naan'
+    if arg1 == 'dough':
+        return 'Naan'
+    else:
+        return 'not_Naan'
 
 
 print(make_dough('cement', 'water'))
@@ -35,18 +38,18 @@ print(make_dough('cement', 'water') == 'not_dough')
 print("got:", make_dough('cement', 'water'))
 
 print("Testing make_dough with 'egg' and 'flour'. Expected --> 'not_dough'")
-print(make_dough('cement', 'water') == 'not_dough')
+print(make_dough('egg', 'flour') == 'not_dough')
 print("got:", make_dough('egg', 'flour'))
 
 # Make test for bake_dough
 # Then, with the dough we should be able to put it in the oven and get out a naan
 print("Testing bake_dough with 'dough'. Expected --> 'Naan'")
 print(bake_dough('dough') == 'Naan')
-print("When calling back_dough, got:", bake_dough('dough'))
+print("When calling bake_dough, got:", bake_dough('dough'))
 
 # When bake_dough is passed something other than 'dough' it should output 'not_Naan'
-# print("My test")
-# print("Testing bake_dough with 'dough'. Expected --> 'Naan'")
-# print(bake_dough('dough') == 'Naan')
-# print("When calling back_dough, got:", bake_dough('dough'))
+print("My test")
+print("Testing bake_dough with 'chicken'. Expected --> 'Naan'")
+print(bake_dough('chicken') == 'not_Naan')
+print("When calling bake_dough, got:", bake_dough('chicken'))
 
